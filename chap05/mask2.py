fa04d3b2cb2d24bcdfef11704ec64743ba707775
dohi <masa0u0masa1215@gmail.com>
2025-03-31 13:33:30 +0900

    edit: mask2.py

--- chap05/mask2.py
+++ chap05/mask2.py
@@ -1,8 +1,3 @@
-#####################################################################
-#
-#####################################################################
-import numpy as np
-
 from videocore6.assembler import qpu
 from videocore6.driver import Driver
 
@@ -20,40 +15,29 @@ def exit_qpu():
 
 @qpu
 def output_test(asm):
-    # uniformの何番目になんの値があるか
-    ADDR0 = 0
-    ADDR1 = 1
-    ADDR2 = 2
-    ADDR3 = 3
-
     # element_number
-    eidx(r2)  # r0 = [0 ... 15]
     mov(r0, 0)
+    eidx(r2)  # r2 = [0 ... 15]
 
     # uniformから値を取り出す
-    # uniformの読み取り位置はインクリメントされる(pop的動作)
     nop(sig=ldunifrf(rf0))
-    sub(null, r2, ADDR0, cond="pushz")
+    sub(null, r2, 0, cond="pushz")
     mov(r0, rf0, cond="ifa")
 
     nop(sig=ldunifrf(rf0))
-    sub(null, r2, ADDR1, cond="pushz")
+    sub(null, r2, 1, cond="pushz")
     mov(r0, rf0, cond="ifa")
 
     nop(sig=ldunifrf(rf0))
-    sub(null, r2, ADDR2, cond="pushz")
+    sub(null, r2, 2, cond="pushz")
     mov(r0, rf0, cond="ifa")
 
     nop(sig=ldunifrf(rf0))
-    sub(null, r2, ADDR3, cond="pushz")
+    sub(null, r2, 3, cond="pushz")
     mov(r0, rf0, cond="ifa")
 
     # アドレスを取り出す
-    nop()
-    rotate(broadcast, r0, -ADDR0)
-    mov(r1, r5)
-
-    # element_number
+    mov(r1, r0)
     eidx(r2)  # r2 = [0 ... 15]
     shl(r2, r2, 2)  # 各数値を4倍
     add(r1, r1, r2)  # result[] のアドレスから ストライド=4バイトのアドレスベクトルを生成
@@ -75,9 +59,9 @@ def main():
 
         unif = drv.alloc(4, dtype="uint32")
         unif[0] = result.addresses()[0]
-        unif[1] = 1
-        unif[2] = 2
-        unif[3] = 3
+        unif[1] = result.addresses()[0] + 1
+        unif[2] = result.addresses()[0] + 2
+        unif[3] = result.addresses()[0] + 3
 
         print("before")
         print(result)
